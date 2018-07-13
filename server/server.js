858326fcdc4e9e39027c5c7b17a847cd0508523d
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2018-07-13 15:56:52 -0500

    configurar para heroku

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const path = require('path'); //rutas entre archivos
 const express= require('express');
 const app = express();
 
-
+var puerto = process.env.PORT || 3000;
 let publicos = path.join(__dirname , '../public');
 console.log(publicos);
 app.use(express.static(publicos));
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000!');
+app.listen(puerto, () => {
+    console.log(`Escuchando desde el puerto ${puerto}`);
 });
 
