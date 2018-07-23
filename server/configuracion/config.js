fab38a5c816a058b96945bc1817d609127f883b3
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2018-07-23 18:29:39 -0500

    reparar process env

--- server/configuracion/config.js
+++ server/configuracion/config.js
@@ -3,8 +3,8 @@ const config = require('./locales.json'); //? json con las configuraciones
 let env  = process.env.NODE_ENV || "desarrollo";
 
 if(env === 'desarrollo' || env === 'test'){
-
-    let entorno = config[env];
+    console.log(config);
+    const entorno = config[env];
     let llaves = Object.keys(entorno);
     llaves.forEach(element => {
         process.env[element] = entorno[element];
